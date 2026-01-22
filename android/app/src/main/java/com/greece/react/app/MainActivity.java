08c280c5c52e594d905687e002f1b0802264090c
wangyue <wangyue@dexmal.com>
2026-01-22 18:04:18 +0800

    app

---
+++ android/app/src/main/java/com/greece/react/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.greece.react.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.greece.react.app',
+  appName: 'greece',
+  webDir: 'build'
+};
+
+export default config;
